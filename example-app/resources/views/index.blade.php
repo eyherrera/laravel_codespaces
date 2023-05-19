20886db5a5da3c3f8942856e61fe0a5137e5c426
eyherrera <106132557+eyherrera@users.noreply.github.com>
2023-05-19 00:23:42 +0000

    created migration

---
+++ example-app/resources/views/index.blade.php
@@ -0,0 +1,29 @@
+
+<html>
+    <header>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </header>
+
+    <head>
+        
+    </head>
+    <body style="background-color: rgb(29, 29, 29);">
+        <h1 style="text-align:center; color:white; margin-top:10px">Hola Mundo</h1>
+        <div class="container" style="margin-top: 10px;">
+            <div class="row">
+              <div class="col-sm-4">
+                <h2 style="color: white;">Column 1</h2>
+                <p style="color: white;">This is the content of the first column.</p>
+              </div>
+              <div class="col-sm-4">
+                <h2 style="color: white;">Column 2</h2>
+                <p style="color: white;">This is the content of the second column.</p>
+              </div>
+              <div class="col-sm-4">
+                <h2 style="color: white;">Column 3</h2>
+                <p style="color: white;">This is the content of the third column.</p>
+              </div>
+            </div>
+          </div>
+    </body>
+</html>
